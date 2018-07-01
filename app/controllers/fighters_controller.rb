566ca4ef218bd13d60578bf7b27ac921f2e178ee
Thu Le <lethikhanhthu@gmail.com>
2018-07-02 00:49:33 +0200

    Add fight performance

--- app/controllers/fighters_controller.rb
+++ app/controllers/fighters_controller.rb
@@ -21,9 +21,27 @@ class FightersController < ApplicationController
   def newfight
     @fighters = Fighter.all
     render "fighters/newfight"
-    def fight_result()
-      
-    end
+  end
+
+  # POST /fighters/newfight
+  def newfight_result
+    @fighters = Fighter.all
+    @fighter1id = params["fighter1"]["id"]
+    @fighter2id = params["fighter2"]["id"]
+    @fighter1 = Fighter.find(@fighter1id)
+    @fighter2 = Fighter.find(@fighter2id)
+    @fight_winner = pick_winner(@fighter1, @fighter2)
+    render "fighters/newfight"
+  end
+
+  def pick_winner(fighter1, fighter2)
+    winner = 
+      if fighter1.strength >= fighter2.strength
+        fighter1
+      else
+        fighter2
+      end
+    return winner
   end
 
   # GET /fighters/1/edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root to: redirect('/fighters')
   get '/fighters/newfight', to: 'fighters#newfight'
+  post '/fighters/newfight', to: 'fighters#newfight_result'
   resources :fighters
-
+  # get 'fighters/history', to: 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
