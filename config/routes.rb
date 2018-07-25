00a2f113bfb5fb590730a47e086a83ada026990d
Thu Le <lethikhanhthu@gmail.com>
2018-07-25 23:52:22 +0200

    Clean up

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/fighters/newfight', to: 'fighters#newfight'
   post '/fighters/newfight', to: 'fighters#newfight_result'
   resources :fighters
-  # get '/fighters/history', to: 
+  # get '/fighters/history', to:
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
